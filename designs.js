9ee55867a480efd08941e24f59c86958aa735dcf
Staci Holmes-Segarra <stacihs@hotmail.com>
2018-05-26 18:59:18 -0500

    Change much of jQuery to vanilla javascript

--- designs.js
+++ designs.js
@@ -1,31 +1,29 @@
-const form = document.querySelector("form"),
-	    table = document.querySelector("table");
+const grid = document.querySelector("table");
+const enter = document.querySelector("input[type='submit']");
+enter.addEventListener("click", function(event) {
+    grid.innerHTML = "";
+    makeGrid();
+    event.preventDefault();
+  });
 
-form.addEventListener("click", function(event) {
-  table.innerHTML = "";
-  makeGrid();
-  event.preventDefault();
-});
-
-function makeGrid(rows, columns) {
-  let rows = document.getElementById("inputHeight").getAttribute("value"),
-      columns = document.getElementById("inputWidth").getAttribute("value");
-  //create rows
-  let i = 0;
-  while (i < rows) {
-    tableRow = document.createElement("tr");
-    table.appendChild(tableRow);
-    i++;
-  //create columns
-    	for (let j = 0; j < columns; j++) {
-          box = document.createElement("td");
-          table.lastchild.appendChild(box);
-        }
-  }
-  //assign box color
-  tableData.addEventListener("click", function() {
-    let colorChoice = document.getElementById("colorPicker").getAttribute("value");
-    box.style.backgroundColor = colorChoice;
-  }):
- };
-}):
+  function makeGrid(row, column) {
+    row = document.getElementById("inputHeight").value;
+    column = document.getElementById("inputWidth").value;
+    //create row
+    let i = 0;
+    while (i < row) {
+      const tr = document.createElement("tr");
+      grid.appendChild(tr);
+      i++;
+      //create column
+          for (let j = 0; j < column; j++) {
+             $("tr:last-of-type").append("<td></td>");
+            }
+      }
+      //assign box color
+    $("td").click(function() {
+      const colorChoice = document.getElementById("colorPicker").value;
+      //td.style.backgroundColor = colorChoice;
+      $(this).css("background-color", colorChoice);
+    });
+  };
